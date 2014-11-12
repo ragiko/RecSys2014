7099eb43b16c097574598ded1a04ca1ae74058aa
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-11-12 11:35:33 +0900

    #2 logの出力完了

--- SparqlClient/src/main/java/org/eswc2014/challenge/lodrecsys/sparql/QueryExecutor.java
+++ SparqlClient/src/main/java/org/eswc2014/challenge/lodrecsys/sparql/QueryExecutor.java
@@ -1,5 +1,9 @@
 package org.eswc2014.challenge.lodrecsys.sparql;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
@@ -7,6 +11,7 @@ import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.ibm.icu.util.StringTokenizer;
 
 public class QueryExecutor {
 
@@ -15,57 +20,58 @@ public class QueryExecutor {
 	private String endpoint = "http://dbpedia.org/sparql";
 	private String graphURI = "http://dbpedia.org";
 
-	public void exec(String resource, String prop) {
+	public String exec(String resource, String prop) {
 		this.resource = resource;
-		this.property=prop;
+		this.property = prop;
 		Query query;
 		String q;
 
 		String resourceQuery = "<" + resource + ">";
 		String propQuery = "<" + prop + ">";
-		// creation of a sparql query for getting all the resources connected to resource
-		//the FILTER isIRI is used to get only resources, so this query descards any literal or data-type
+		// creation of a sparql query for getting all the resources connected to
+		// resource
+		// the FILTER isIRI is used to get only resources, so this query
+		// descards any literal or data-type
 
-		q = "SELECT ?o WHERE { " 
-			+ resourceQuery + " " + propQuery +
-			" ?o. }";
-		
-		System.out.println("executing query  : " + q.toString());
-		
+		q = "SELECT ?o WHERE { " + resourceQuery + " " + propQuery + " ?o. }";
+
 		try {
 			query = QueryFactory.create(q);
 
-			execQuery(query);
+			return execQuery(query);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+		
+		return "";
 
 	}
+
 	public void exec(String resource) {
 		this.resource = resource;
 		Query query;
 		String q;
 
 		String resourceQuery = "<" + resource + ">";
-		// creation of a sparql query for getting all the resources connected to resource
-		//the FILTER isIRI is used to get only resources, so this query descards any literal or data-type
+		// creation of a sparql query for getting all the resources connected to
+		// resource
+		// the FILTER isIRI is used to get only resources, so this query
+		// descards any literal or data-type
 
-		q = " SELECT * WHERE {{" + " ?s ?p " + resourceQuery
-				+ ".   " + "FILTER isIRI(?s). " + " } UNION {" + resourceQuery + 
-				 " ?p ?o " + "FILTER isIRI(?o). " + "}}";
+		q = " SELECT * WHERE {{" + " ?s ?p " + resourceQuery + ".   "
+				+ "FILTER isIRI(?s). " + " } UNION {" + resourceQuery
+				+ " ?p ?o " + "FILTER isIRI(?o). " + "}}";
 		try {
 			query = QueryFactory.create(q);
 
-			execQuery(query);
+			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 
 	}
 
-	private void execQuery(Query query) {
-
-		System.out.println("executing query  : " + query.toString());
+	private String execQuery(Query query) {
 
 		QueryExecution qexec = null;
 		try {
@@ -80,23 +86,22 @@ public class QueryExecutor {
 			QuerySolution qs;
 			RDFNode node, prop;
 
-			String n="", p=this.property;
+			String n = "", p = this.property;
 
-			System.out.println("Results:");
-			//iteration over the resultset
+			// iteration over the resultset
 			while (results.hasNext()) {
 
 				qs = results.next();
-				
-				if (qs.contains("p")){
-					prop = qs.get("p"); //get the predicate of the triple
+
+				if (qs.contains("p")) {
+					prop = qs.get("p"); // get the predicate of the triple
 					p = prop.toString();
 					p = p.replace("<", "");
 					p = p.replace(">", "");
-					
+
 				}
 				if (qs.get("o") == null) {
-					node = qs.get("s"); //get the subject of the triple
+					node = qs.get("s"); // get the subject of the triple
 					n = node.toString();
 					n = n.replace("<", "");
 					n = n.replace(">", "");
@@ -104,12 +109,13 @@ public class QueryExecutor {
 					System.out.println(n + '\t' + p + '\t' + resource);
 				} else {
 
-					node = qs.get("o"); //get the object of the triple
+					node = qs.get("o"); // get the object of the triple
 					n = node.toString();
 					n = n.replace("<", "");
 					n = n.replace(">", "");
 
-					System.out.println(resource + '\t' + p + '\t' + n);
+					return n;
+					//System.out.println(n);
 
 				}
 
@@ -118,19 +124,38 @@ public class QueryExecutor {
 		} finally {
 			qexec.close();
 		}
+		
+		return "";
 
 	}
 
 	public static void main(String[] args) {
 
-		QueryExecutor exec = new QueryExecutor();
-		//get all the triples related to the predicate http://dbpedia.org/ontology/starring
-		//wherein the Godfather appears as subject or object	
-		exec.exec("http://dbpedia.org/resource/Orphans_of_Chaos","http://dbpedia.org/ontology/abstract");
+
 		
-		//get all the triples that involve the Godfather	
-		// exec.exec("http://dbpedia.org/resource/The_Godfather");
+	
+		// ファイルを読み込む
+		try {
+			String path = "/Users/tag/programing/RecSys2014_re/DBbook/DBbook_Items_DBpedia_mapping_no_col.tsv";
+			BufferedReader br = new BufferedReader(new FileReader(path));
+
+			String line;
+			QueryExecutor exec = new QueryExecutor();
+			
+			while ((line = br.readLine()) != null) {
+				String[] r = line.split("\t");
+				String url = r[2];
 				
+				String s = exec.exec(url, "http://dbpedia.org/ontology/abstract");
+				System.out.println(r[0] + "\t" + r[1] + "\t" + r[2] + "\t" + s);
+				
+			}
+
+			// 終了処理
+			br.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 
 	}
 }
